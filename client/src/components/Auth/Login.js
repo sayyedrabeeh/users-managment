5e5e06db0c1c0fa94a7dca6930da3de0ee70cb4b
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-27 01:01:44 +0530

    Add login csss

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { setAuth } from '../../redux/slices/authSlice'
+import './Login.css'
 
 
 export default function Login() {
