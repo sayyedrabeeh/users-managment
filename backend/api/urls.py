5416f5b841378f71c85c4e9ded7f0836de2e46e7
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-26 23:49:20 +0530

    fix view for api urls  in backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
 
  
 from django.urls import path 
- 
+ from .views import RegisterView, LoginView, ProfileView, AdminUserListView
 
 urlpatterns = [
     
-    
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('profile/', ProfileView.as_view()),
+    path('admin/users/', AdminUserListView.as_view()),
 ]  
