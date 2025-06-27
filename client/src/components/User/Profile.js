cf22746106d041a404e4eb7718ee350abab40f03
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-27 06:50:06 +0530

    feat profile.js  added

---
+++ client/src/components/User/Profile.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import axios from 'axios'
+
+export default function Profile() {
+  const { token, user } = useSelector(state => state.auth)
+  const [img, setImg] = useState(null)
+
+  const handleUpload = async (e) => {
+    const file = e.target.files[0]
+    const formData = new FormData()
+    formData.append('profile_image', file)
+
+    const res = await axios.put('http://localhost:8000/api/profile/', formData, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+
+    alert('Updated!')
+  }
+
+  return (
+    <div>
+      <h2>Profile of {user.username}</h2>
+      <input type="file" onChange={handleUpload} />
+      <img src={`http://localhost:8000${user.profile_image}`} width={150} />
+    </div>
+  )
+}
